40b9807f44a9c927ec902561df81a8a75a4c49d3
dirtydoll <100242607+dirtydoll@users.noreply.github.com>
2024-07-17 11:08:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+ # 1st program
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+a1=1234
+b1=5678
+a= (a1%1000 - a1%10)//10
+b= (b1%1000 - b1%10)//10
+print(a+b)
+#4th program
+d=13.42
+e=42.13
+d1=int(d)
+e1=int(e)
+d2= round(d%1,2)*100
+e2= round(e%1,2)*100
+print(d1==e2 or e1==d2)
